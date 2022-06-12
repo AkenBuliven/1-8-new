0ee0662409a6b86ff63853c57cfb82fc68e36d32
AkenBuliven <101834092+AkenBuliven@users.noreply.github.com>
2022-06-12 19:21:36 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) { 
+	
+  int k,m;
+  scanf("%d%d", &k,&m);
+
+	if (k < 1) k = 1;
+
+  for (int i = k; i <= m; i++)
+{
+	printf (" %d", i);
+}
+  return 0;
+}
